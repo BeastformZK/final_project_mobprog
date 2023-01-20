aed948e6d0d35f3b4572eb17e1a892d3a47254da
gabb123 <franz.gabz13@gmail.com>
2023-01-20 11:50:46 +0800

    post form improvement commit

--- lib/constant_variables/constants.dart
+++ lib/constant_variables/constants.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 
-const baseURL = 'http://192.168.68.46:8000/api';
+const baseURL = 'http://192.168.18.149:8000/api';
 const loginURL = '$baseURL/login';
 const registerURL = '$baseURL/register';
 const logoutURL = '$baseURL/logout';
